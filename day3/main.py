f2e3ccac0473f8d07e236402a0eedcee3883025e
GraceHopper0012 <145455023+GraceHopper0012@users.noreply.github.com>
2025-12-03 14:32:03 +0100

    day3 working

--- day3/main.py
+++ day3/main.py
@@ -1,12 +1,28 @@
-largest_num = 0
-idx_largest = 0
-for idx, battery in enum(batteries[:-1]):
-    if int(battery) > largest_num:
-        largest_num = battery
-        idx_largest = idx
-        if largest_num == 9:
-            break
-largest_second_num = 0
-for battery in batteries[idx_largest:]:
-    if int(battery > largest_second_num):
-        largest_second_num = battery
+def get_largest_joltage(batteries):
+    largest_num = 0
+    idx_largest = 0
+    for idx, battery in enumerate(batteries[:-1]):
+        if int(battery) > largest_num:
+            largest_num = int(battery)
+            idx_largest = idx
+            if largest_num == 9:
+                break
+    largest_second_num = 0
+    for battery in batteries[idx_largest+1:]:
+        if int(battery) > largest_second_num:
+            largest_second_num = int(battery)
+    return int(str(largest_num) + str(largest_second_num))
+
+def parse_input(filename):
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+        return lines
+
+def __main__():
+    batterypacks = parse_input("input.txt")
+    max_joltage = 0
+    for batterypack in batterypacks:
+        max_joltage += get_largest_joltage(batterypack)
+    print(max_joltage)
+
+__main__()
